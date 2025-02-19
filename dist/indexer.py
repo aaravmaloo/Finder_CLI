2235e0cf7bae8d7f6263d8275e24f5fe92d28c3a
Aarav Maloo <aaravmaloov@gmail.com>
2025-02-19 20:41:28 +0530

    get number of processors in the CPU, half it and have them as max_workers

--- dist/indexer.py
+++ dist/indexer.py
@@ -10,11 +10,13 @@ index_file = os.path.join(finder_cli_dir, "index.txt")
 os.makedirs(finder_cli_dir, exist_ok=True)
 
 
+
+
 def scan_directory(path):
 
     file_paths = []
     for root, _, files in os.walk(path):
-        # Skip AppData directory
+
         if "AppData" in root:
             continue
 
@@ -24,13 +26,20 @@ def scan_directory(path):
     return file_paths
 
 
+
 def threaded_scan():
     subdirs = [os.path.join(home_dir, d) for d in os.listdir(home_dir)]
-    subdirs = [d for d in subdirs if os.path.isdir(d) and "AppData" not in d]  # Exclude AppData
+    subdirs = [d for d in subdirs if os.path.isdir(d) and "AppData" not in d]
 
     file_paths = []
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+
+    cpu_count_threads = os.cpu_count()
+    cpu_count_threads_final_num = int(os.cpu_count()/2)
+    print(cpu_count_threads_final_num)
+
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=cpu_count_threads_final_num) as executor:
         results = executor.map(scan_directory, subdirs)
 
         for result in results:

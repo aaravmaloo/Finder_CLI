91cf85517dfca3f6b52114a3e496d9cb6c362fcf
Aarav Maloo <aaravmaloov@gmail.com>
2025-02-19 20:36:03 +0530

    removed duplicated name in main function.

--- dist/indexer.py
+++ dist/indexer.py
@@ -39,12 +39,3 @@ def threaded_scan():
     return file_paths
 
 
-if __name__ == "__main__":
-    print(f"Scanning your home directory: {home_dir} using 5 threads... (Skipping AppData)")
-    all_files = threaded_scan()
-
-    with open(index_file, "w", encoding="utf-8") as f:
-        f.write("\n".join(all_files))
-
-    print(f"Scan complete! Indexed {len(all_files)} files.")
-    print(f"Index saved at: {index_file}")

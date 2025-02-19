2586e64eeddfba14629e7752af83781cc2d47d64
Aarav Maloo <aaravmaloov@gmail.com>
2025-02-19 20:34:56 +0530

    temporary: added if name in main for implementing fuzzy_search.py(alpha version)

--- dist/fuzzy_search.py
+++ dist/fuzzy_search.py
@@ -2,3 +2,26 @@ import keyboard
 import os
 import indexer
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print(f"Scanning your home directory: {indexer.home_dir} using 5 threads... (Skipping AppData)")
+    all_files = indexer.threaded_scan()
+
+    with open(indexer.index_file, "w", encoding="utf-8") as f:
+        f.write("\n".join(all_files))
+
+    print(f"Scan complete! Indexed {len(all_files)} files.")
+    print(f"Index saved at: {indexer.index_file}")

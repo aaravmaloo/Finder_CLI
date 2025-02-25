83fd2108bd96148b05c8a5558aa010a3ee6dc52b
Aarav Maloo <aaravmaloov@gmail.com>
2025-02-25 20:08:21 +0530

    removed comments for everything

--- dist/interface.py
+++ dist/interface.py
@@ -11,9 +11,9 @@ import indexer
 
 def init_colors():
     curses.start_color()
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)  # Red text
-    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)  # Green text
-    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)  # Default white text
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
 @lru_cache(maxsize=128)
 def format_size(size):
